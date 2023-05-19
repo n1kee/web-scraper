c3f71720c90a5e5afc98822731f99bb0b3381fbb
Your Name <you@example.com>
2023-05-19 07:19:41 +0300

    +DEPENDENCIES

--- src/WebScraperBundle.php
+++ src/WebScraperBundle.php
@@ -2,7 +2,7 @@
 
 namespace WebScraperBundle;
 
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
  * A bundle for web scraping.
  *
  */
-class WebScraperBundle extends AbstractBundle {
+class WebScraperBundle extends Bundle {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import(__DIR__ . '/../config/services.yaml');
